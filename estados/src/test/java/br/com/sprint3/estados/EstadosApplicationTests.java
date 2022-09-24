370005616c1f773d0fb9aa68baa7ab28263d89c7
José Augusto Neto <netiin00@gmail.com>
2022-09-24 20:29:09 -0300

    Criação do projeto, implementação de pacotes e classes

---
+++ estados/src/test/java/br/com/sprint3/estados/EstadosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.sprint3.estados;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstadosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
