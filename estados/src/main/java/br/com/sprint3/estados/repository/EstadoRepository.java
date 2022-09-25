c716b2bdd84b3dcad3ffc3b7f39f23c7cb9bb930
José Augusto Neto <netiin00@gmail.com>
2022-09-25 18:03:29 -0300

    Criando forms de usuario e estado e adicionando data.sql para facilitar o desenvolvimento

--- estados/src/main/java/br/com/sprint3/estados/repository/EstadoRepository.java
+++ estados/src/main/java/br/com/sprint3/estados/repository/EstadoRepository.java
@@ -1,9 +1,16 @@
 package br.com.sprint3.estados.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.com.sprint3.estados.model.Estado;
+import br.com.sprint3.estados.model.enums.Regiao;
 
 public interface EstadoRepository extends JpaRepository<Estado, Long>{
+
+	List<Estado> findByRegiao(Regiao regiao);
+
+
 	
 }
