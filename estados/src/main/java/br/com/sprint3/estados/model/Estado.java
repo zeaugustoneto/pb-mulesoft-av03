e6dc0df5604019f47599019ced415ee0f3a2e25b
José Augusto Neto <netiin00@gmail.com>
2022-09-24 21:50:47 -0300

    Criando DTO de usuario e estado, criando endpoints de listar todos usuarios e todos estados

---
+++ estados/src/main/java/br/com/sprint3/estados/model/Estado.java
@@ -0,0 +1,83 @@
+package br.com.sprint3.estados.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import br.com.sprint3.estados.model.enums.Regiao;
+
+@Entity
+public class Estado {
+	
+	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String nome;
+	
+	private Regiao regiao;
+	private int populacao;
+	private String capital;
+	private double area;
+	
+
+	
+	public Estado(String nome, Regiao regiao, int populacao, String capital, double area) {
+		
+		this.nome = nome;
+		this.regiao = regiao;
+		this.populacao = populacao;
+		this.capital = capital;
+		this.area = area;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Regiao getRegiao() {
+		return regiao;
+	}
+
+	public void setRegiao(Regiao regiao) {
+		this.regiao = regiao;
+	}
+
+	public int getPopulacao() {
+		return populacao;
+	}
+
+	public void setPopulacao(int populacao) {
+		this.populacao = populacao;
+	}
+
+	public String getCapital() {
+		return capital;
+	}
+
+	public void setCapital(String capital) {
+		this.capital = capital;
+	}
+
+	public double getArea() {
+		return area;
+	}
+
+	public void setArea(double area) {
+		this.area = area;
+	}
+	
+	
+	
+}
---
+++ estados/src/main/java/br/com/sprint3/estados/model/enums/Regiao.java
@@ -0,0 +1,9 @@
+package br.com.sprint3.estados.model.enums;
+
+public enum Regiao {
+	Norte,
+	Nordeste,
+	Centro_Oeste,
+	Sul,
+	Sudeste
+}
