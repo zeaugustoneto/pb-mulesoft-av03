aa69cde84cd5182d49668baf2af4b859cd83d923
José Augusto Neto <netiin00@gmail.com>
2022-09-26 05:37:45 -0300

    Adicionando e configurando o Spring Security

---
+++ estados/src/main/java/br/com/sprint3/estados/config/security/SecurityConfigurations.java
@@ -0,0 +1,11 @@
+package br.com.sprint3.estados.config.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfigurations extends WebSecurityConfigurerAdapter{
+
+}
